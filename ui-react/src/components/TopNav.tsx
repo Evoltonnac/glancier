78e36975fbf69771891013fc8b22b324f803a1ec
Evoltonnac <1173985303@qq.com>
2026-03-16 23:20:03 +0800

    fix(ui): export LogoIcon from TopNav for reuse
    The LogoIcon component was defined but not exported, causing
    import errors in Settings.tsx About page.

--- ui-react/src/components/TopNav.tsx
+++ ui-react/src/components/TopNav.tsx
@@ -10,7 +10,7 @@ import { useI18n } from "../i18n";
 
 import { AppHeader } from "./AppHeader";
 
-const LogoIcon = ({ className }: { className?: string }) => (
+export const LogoIcon = ({ className }: { className?: string }) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="15 15 70 70"
